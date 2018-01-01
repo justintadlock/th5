344f4a9d338c7db3c64f894848ef91535c15e2ab
Justin Tadlock <justintadlock@gmail.com>
2018-01-01 10:46:41 -0600

    Remove discount code notice from `header.php`.

--- header.php
+++ header.php
@@ -9,16 +9,6 @@
 
 	<div class="site">
 
-<?php if ( ! current_user_can( 'view_club_content' ) ) : ?>
-
-<div class="notice">
-
-	<p class="notice__message">Christmas special! Get 25% off your entire purchase by using the <strong>XMAS2017</strong> discount code at checkout.</p>
-
-</div>
-
-<?php endif; ?>
-
 		<header class="site-header">
 
 			<div class="site-header__wrap">
